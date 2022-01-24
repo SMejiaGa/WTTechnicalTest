b7a63656744268398de252750d964f7e2f0c896c
SMejiaGa <sebasmejia084@gmail.com>
2022-01-23 23:47:40 -0500

    Added google maps

--- WTTechnicalTest/Config/AppDelegate.swift
+++ WTTechnicalTest/Config/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         prepareUIWindow()
+        GMSServices.provideAPIKey("AIzaSyA_DaIs4tvv0-uGxXB_jYXLTwkSYcGwSGo")
         return true
     }
     
--- WTTechnicalTest/Modules/Contact/View/ContactViewController.swift
+++ WTTechnicalTest/Modules/Contact/View/ContactViewController.swift
@@ -6,16 +6,19 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ContactViewController: UIViewController {
     @IBAction private func backButton() {
         self.navigationController?.popViewController(animated: true)
     }
+    
     private let viewModel: ContactViewModel
     
     init(viewModel: ContactViewModel) {
         self.viewModel = viewModel
         super.init(nibName: String(describing: Self.self), bundle: nil)
+        setupMap()
     }
     
     required init?(coder: NSCoder) {
@@ -24,8 +27,20 @@ class ContactViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.isNavigationBarHidden = true  
-
-        // Do any additional setup after loading the view.
+        self.navigationController?.isNavigationBarHidden = true
+        
+    }
+    
+    private func setupMap() {
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect(x: 0, y: 500, width: 500, height: 400), camera: camera)
+        self.view.addSubview(mapView)
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        mapView.settings.myLocationButton = true
     }
 }
